96a254cab43a60955ad84ff8c6ef1267498bd723
cin-derella <shirley_cinderella@outlook.com>
2019-09-30 21:33:13 +0800

    SWReveal

---
+++ appsmack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  appsmack
+//
+//  Created by Dante on 2019/9/30.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+    
+
+}
