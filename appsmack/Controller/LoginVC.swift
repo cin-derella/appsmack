12c55e7bb40d6a863adae24777c927cc2cb001a2
cin-derella <shirley_cinderella@outlook.com>
2019-10-01 00:21:57 +0800

    LoginVC

---
+++ appsmack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  appsmack
+//
+//  Created by Dante on 2019/10/1.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+     }
+
+}
---
+++ appsmack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  appsmack
+//
+//  Created by Dante on 2019/9/30.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
