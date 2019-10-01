b7478e8becf993fe375429fffa0d5a803752e5d9
cin-derella <shirley_cinderella@outlook.com>
2019-10-01 15:23:57 +0800

    CreateAccountVC

---
+++ appsmack/Controller/CreateAccountVC.swift
@@ -0,0 +1,28 @@
+//
+//  CreateAccountVC.swift
+//  appsmack
+//
+//  Created by Dante on 2019/10/1.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+    
+  
+      
+    
+    @IBAction func closePreessed(_ sender: Any) {
+      performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+  
+}
