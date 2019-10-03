886cb032c1620fcda672acfed60de9ee22552445
cin-derella <shirley_cinderella@outlook.com>
2019-10-03 14:15:52 +0800

    loggin

--- appsmack/Controller/ChannelVC.swift
+++ appsmack/Controller/ChannelVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+
+    @IBOutlet weak var userImg: CircleImage!
     
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue:UIStoryboardSegue){}
@@ -17,6 +19,8 @@ class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth=self.view.frame.size.width-60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
@@ -24,4 +28,16 @@ class ChannelVC: UIViewController {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
+    @objc func userDataDidChange(_ notif:Notification){
+        if AuthService.instance.isLoggedIn{
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named:UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(compoents: UserDataService.instance.avatarColor)
+        }else{
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named:"menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+            
+        }
+    }
 }
