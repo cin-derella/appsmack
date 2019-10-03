0addf0c4447cb9d7d869d16cd84eda30f29186b8
cin-derella <shirley_cinderella@outlook.com>
2019-10-03 12:04:52 +0800

    random bgcolor

--- appsmack/Utilities/Constants.swift
+++ appsmack/Utilities/Constants.swift
@@ -16,6 +16,12 @@ let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 
+//Colors
+let cinPurplePlaceholder = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 0.5)
+
+//Notification Constants
+let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
